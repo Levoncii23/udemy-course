06f0b3ee9c11a91a0b8703ab9c892471af3c9419
Levoncii23 <levoncii23@gmail.com>
2021-01-12 14:24:55 +0300

    classwork with udemy rateoffilm

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
 "use strict";
-   
-const user = "Ivan";
 
-alert(`Привет, ${user}`);
+let numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
+let lastFilm = prompt("Один из последних просмотренных фильмов?"); 
+let rateFilm = prompt("На сколько оцените его?")
+let ownFilm = lastFilm + " : " + rateFilm
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: [ownFilm],
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+console.log("Последние фильмов:" + numberOfFilms + " " + personalMovieDB["movies"]);
