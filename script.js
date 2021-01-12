950046e1a26745fb60dd5a465e28ca6d2939d010
Levoncii23 <levoncii23@gmail.com>
2021-01-12 14:37:15 +0300

    delete 012 script work done

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
-let lastFilm = prompt("Один из последних просмотренных фильмов?"); 
-let rateFilm = prompt("На сколько оцените его?")
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", '');
+const lastFilm = prompt("Один из последних просмотренных фильмов?", ''), 
+    rateFilm = prompt("На сколько оцените его?", ''),
+    lastFilm2 = prompt("Один из последних просмотренных фильмов?", ''), 
+    rateFilm2 = prompt("На сколько оцените его?", '');
+    
 let ownFilm = lastFilm + " : " + rateFilm
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: [ownFilm],
+    movies: {},
     actors: {},
     genres: [],
-    privat: false,
+    privat: false
 };
 
-console.log("Последние фильмов:" + numberOfFilms + " " + personalMovieDB["movies"]);
+personalMovieDB.movies[lastFilm] = rateFilm;
+personalMovieDB.movies[lastFilm2] = rateFilm2;
+
+console.log(personalMovieDB);
